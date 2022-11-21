c520380b6f6f8b6f5a55752e2002a1b8ad3546b1
Nikki Lemon <nikki.lemon93@gmail.com>
2022-11-21 11:32:33 +0000

    add play_again function

--- run.py
+++ run.py
@@ -20,14 +20,16 @@ pixar_movies = SHEET.worksheet('pixar_movies')
 
 words_data = word.get_all_values()
 
+chosen_word = ""
+
 def get_word():
-    word = random.choice(words_data)
-    print(word)
-    return word
+    chosen_word = random.choice(words_data)
+    return chosen_word[0]
 
 
-def play(word):
-    word_completion = "_" * len(word)
+def play(chosen_word):
+    print(type(chosen_word))
+    word_completion = "_" * len(chosen_word)
     guessed = False
     guessed_letters = []
     guessed_words = []
@@ -39,35 +41,35 @@ def play(word):
     print("\n")
 
     while not guessed and tries > 0:
-        guess = input("Please guess a letter or word: ")
+        guess = input("Please guess a letter or word: ").lower()
+        print(guessed_letters)
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
                 print("You already guessed the letter", guess)
-            elif guess not in word:
+            elif guess not in chosen_word:
                 print(guess, "is not in the word.")
                 tries -= 1
                 guessed_letters.append(guess)
-                print(guessed_letters)
             else:
                 print("Good job,", guess, "is in the word!")
                 guessed_letters.append(guess)
                 word_as_list = list(word_completion)
-                indices = [i for i, letter in enumerate(word) if letter == guess]
+                indices = [i for i, letter in enumerate(chosen_word) if letter == guess]
                 for index in indices:
                     word_as_list[index] = guess
                 word_completion = "".join(word_as_list)
                 if "_" not in word_completion:
                     guessed = True
-        elif len(guess) == len(word) and guess.isalpha():
+        elif len(guess) == len(chosen_word) and guess.isalpha():
             if guess in guessed_words:
                 print("You already guessed the word", guess)
-            elif guess != word:
+            elif guess != chosen_word:
                 print(guess, "is not the word.")
                 tries -= 1
                 guessed_words.append(guess)
             else:
                 guessed = True
-                word_completion = word
+                word_completion = chosen_word
         else:
             print("Not a valid guess.")
         print(display_hangman(tries))
@@ -77,16 +79,21 @@ def play(word):
         print("Congrats, you guessed the word! You win!")
     else:
         print("Sorry, you ran out of tries. The word was ")
-        print(word)
+        print(chosen_word)
         print("Try again next time")
+
+def play_again():
+    while input("Play Again? (Y/N) ").upper() == "Y":
+        chosen_word = get_word()
+        play(chosen_word)
 
 
 def main():
-    word = get_word()
-    play(word)
-    while input("Play Again? (Y/N) ").upper() == "Y":
-        word = get_word()
-        play(word)
+    chosen_word = get_word()
+    print(chosen_word)
+    play(chosen_word)
+    play_again()
+
 
 
 if __name__ == "__main__":
